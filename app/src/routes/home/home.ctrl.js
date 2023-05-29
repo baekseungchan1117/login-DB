b84ed0a1353601c4d435deadbc4409aadbf78f23
baekseungchan1117 <bsc7386@gmail.com>
2023-05-30 02:36:44 +0900

    모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const UserStorage = require('../../models/UserStorage')
+const User = require('../../models/User')
 
 
 const output = {
@@ -17,27 +17,32 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        const id  = req.body.id
-        const psword = req.body.psword
+        const user = new User(req.body);
+        const response = user.login();
+        // console.log(response);
+        return res.json(response);
 
-        // const userStorage  = new UserStorage() ; -> 인스턴스화 하지 않고 바로 Class에 static을 추가하여 바로 사용가능
-        // console.log(UserStorage.users); // -> class에 static을 하지 않으면 undefined 나옴
-        // console.log(id);
-        // console.log(psword);
+        // const id  = req.body.id
+        // const psword = req.body.psword
 
-        const users = UserStorage.getUsers("id", "psword")  // 내가 필요한 데이터를 가져올 수 있다.
+        // // const userStorage  = new UserStorage() ; -> 인스턴스화 하지 않고 바로 Class에 static을 추가하여 바로 사용가능
+        // // console.log(UserStorage.users); // -> class에 static을 하지 않으면 undefined 나옴
+        // // console.log(id);
+        // // console.log(psword);
 
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인을 실패하였습니다."
-        return res.json(response)
+        // const users = UserStorage.getUsers("id", "psword")  // 내가 필요한 데이터를 가져올 수 있다.
+
+        // const response = {};
+        // if(users.id.includes(id)){
+        //     const idx = users.id.indexOf(id);
+        //     if(users.psword[idx] === psword){
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
+        // response.success = false;
+        // response.msg = "로그인을 실패하였습니다."
+        // return res.json(response)
     }
 }
 
